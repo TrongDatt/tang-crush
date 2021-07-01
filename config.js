389defe1be060cbf6552cca7ab24653ddc751d6d
TrongDatt <86768549+TrongDatt@users.noreply.github.com>
2021-07-01 14:59:28 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Babeeeeeeeeeeee à!',
+    introTitle: 'Kong chúa Mai Lan còi à <3!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
+    btnIntro: 'Chó Lan kkk',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
+    btnYes: 'Yêu Trọng Đạt Đẹp Trai lắmmmm <33',
+    btnNo: 'Hoonggg, ghét Tró Đạt nứm :3',
+    question:'Oaaaaa, thích thế. Vậy thì hông được buông bàn tay đâu đấy nha EmIu moazzz moazz <3',
+    btnReply: '',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    messDesc: 'Đưa tay đây nào. Mãi bên nhau Còi nhớ.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/messages/t/100022401227319/' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
